c6ca0cd5eb91cd743dc5bbe0a889bcd0db464564
OlehDubovyi <oleg07011985@gmail.com>
2026-05-27 13:10:37 +0300

    deploy

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import {
   hideLoadMoreButton,
 } from './js/render-functions.js';
 
-const searchForm = document.querySelector('#search-form');
+const searchForm = document.querySelector('.form');
 const loadMoreBtn = document.querySelector('#load-more');
 
 let query = '';
